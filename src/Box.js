0b0352b2780d7eae171c13c2bbde60dbedbb1fce
elena-vost <angara1298@gmail.com>
2021-02-18 18:03:56 +0100

    box style

--- src/Box.js
+++ src/Box.js
@@ -1,1 +1,21 @@
 import React, { Component } from "react";
+import './Box.css'
+
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: "purple" };
+    this.handleClick = rhis.handkeClick.bind(this);
+  }
+  handleClick(){
+
+  }
+  render() {
+    return (
+      <div className="Box" style={{ backgroundColor: this.state.color } onClick=
+      {this.handkeClick}}></div>
+    );
+  }
+}
+
+export default Box;
